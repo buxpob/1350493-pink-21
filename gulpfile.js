839a5c0de1c2ac77559b3b4f05cf6277ca876fa5
buxpob <buxpob.xax@gmail.com>
2021-01-26 21:43:47 +0300

    Правки

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ const styles = () => {
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(gulp.dest("build"))
+    .pipe(gulp.dest("build/css"))
     .pipe(postcss([
       autoprefixer(),
       csso()
